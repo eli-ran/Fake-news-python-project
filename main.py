20dc7be8c75160f7800932e0a2ef699a18e107ce
ellie-ran <rankovae@gmail.com>
2022-11-20 23:40:12 +0100

    Plotting speaker frequency
    Playing around with analyzing the data – how often a speaker appears in the dataset

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 # This is a sample Python script.
 import pandas as pd
+from matplotlib import pyplot as plt
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+liar = pd.read_csv('liar_dataset/train.tsv', sep='\t', header=None, usecols = [0, 1 , 2, 4])
+liar.columns =['id', 'label', 'statement', 'speaker']
+#liar.columns =['id', 'label', 'statement', 'subject', 'speaker', 'jib_title', 'state', 'party', 'subject', 'bt-count', 'ht-count', 'mt-count', 'pof-count', 'context']
 
+plot_size = plt.rcParams["figure.figsize"]
+print(plot_size[0])
+print(plot_size[1])
 
-print('Hello world')
-print('goodbye world ;( ')
-print('do you work now?')
+plot_size[0] = 8
+plot_size[1] = 6
+plt.rcParams["figure.figsize"] = plot_size
 
-liar = pd.read_csv('liar_dataset/train.tsv', sep='\t', header=None, usecols = [0, 1 , 2])
-liar.columns =['id', 'label', 'statement']
+#liar.speaker.value_counts().plot(kind='pie', autopct='%1.0f%%')
 
-print(liar)
+speakerCount = liar.speaker.value_counts()
+speakerCountDF = speakerCount.to_frame().reset_index()
+speakerCountDF.columns = ['name','count']
+#print(labels)
+fig1, ax1 = plt.subplots()
+ax1.pie(speakerCount[0:10], labels = speakerCountDF.name[0:10],autopct='%1.1f%%', startangle=90)
+#ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
+plt.show()
 
-
-
+#print(speakerCount)
+# create a new dataset, deleting the unnecessary columns
